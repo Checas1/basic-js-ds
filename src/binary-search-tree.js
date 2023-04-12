512ce81ed0a9d454a88670dc1e96b000ca83590b
checas1 <checas104@gmail.com>
2023-04-12 19:40:13 +0300

    feat: add binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,110 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js")
+const { Node } = require("../extensions/list-tree.js")
 
-// const { Node } = require('../extensions/list-tree.js');
-
-/**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
 class BinarySearchTree {
+  constructor() {
+    this.branch = null
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let tmp = this.branch
+    return tmp
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    function addThreeSheet(tmp, data) {
+      if (!tmp) return new Node(data)
+      if (tmp.value == data) return tmp
+      data > tmp.data ?
+        tmp.right = addThreeSheet(tmp.right, data) :
+          tmp.left = addThreeSheet(tmp.left, data)
+      return tmp
+    }
+    this.branch = addThreeSheet(this.branch, data)
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    function branchSeek(tmp, data) {
+      if (!tmp) return false
+      if (tmp.data == data) return true
+      if (data > tmp.data){
+        return branchSeek(tmp.right, data)
+      }else{
+        return branchSeek(tmp.left, data)
+      }
+    }
+    let func = branchSeek(this.branch, data)
+    return func
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    function branchSeek(tmp, data) {
+      if (!tmp) return null
+      if (tmp.data === data) return tmp
+      if (data > tmp.data){
+        return branchSeek(tmp.right, data)
+      }else{
+        return branchSeek(tmp.left, data)
+      }
+    }
+    let func = branchSeek(this.branch, data)
+    return func
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    function branchDelete(tmp, data) {
+      if (!tmp) return null
+      if (tmp.data > data){
+        tmp.left = branchDelete(tmp.left, data)
+        return tmp
+      }else if (data > tmp.data){
+        tmp.right = branchDelete(tmp.right, data)
+        return tmp
+      } else {
+        let notRight = !tmp.right
+        let notLeft = !tmp.left
+        let minSheet = tmp.left
+        
+        if (notRight) {
+          tmp = tmp.left
+          return tmp;
+        }
+        if (notLeft) {
+          tmp = tmp.right
+          return tmp
+        }
+        if (notRight && notLeft) return null
+
+        while (minSheet.right) {
+          minSheet = minSheet.right
+        }
+        tmp.data = minSheet.data
+        tmp.left = branchDelete(tmp.left, minSheet.data)
+        return tmp
+      }
+    }
+    this.branch = branchDelete(this.branch, data);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let minSheet = this.branch
+    while (minSheet.left) {
+      minSheet = minSheet.left
+    }
+    let tmp = minSheet.data
+    return tmp
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let maxSheet = this.branch;
+    while (maxSheet.right) {
+      maxSheet = maxSheet.right
+    }
+    let tmp = maxSheet.data
+    return tmp
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
